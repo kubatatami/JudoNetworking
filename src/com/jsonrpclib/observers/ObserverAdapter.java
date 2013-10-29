63bdc73609407762f4fe38e7c3def8e3ffcd7441
Jakub Bogacki <jbogacki@MacBook-Pro-Kuba.local>
2013-10-29 09:02:30 +0100

    observer adapter

---
+++ src/com/jsonrpclib/observers/ObserverAdapter.java
@@ -0,0 +1,45 @@
+package com.jsonrpclib.observers;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbogacki
+ * Date: 25.10.2013
+ * Time: 10:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class ObserverAdapter<T> extends ArrayAdapter<T> {
+
+    protected int resource;
+    protected ObserverAdapterHelper adapterHelper;
+
+    public ObserverAdapter(Context context, int resource) {
+        super(context, resource);
+        this.resource =resource;
+        adapterHelper = new ObserverAdapterHelper(context);
+    }
+
+    public ObserverAdapter(Context context, int resource, T[] objects) {
+        super(context, resource, objects);
+        this.resource = resource;
+        adapterHelper = new ObserverAdapterHelper(context);
+    }
+
+    public ObserverAdapter(Context context, int resource, List<T> objects) {
+        super(context, resource, objects);
+        this.resource = resource;
+        adapterHelper = new ObserverAdapterHelper(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return adapterHelper.getView(resource,getItem(position),convertView,parent);
+    }
+
+}
