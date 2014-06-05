f96ebf3befa6886d171a6244418ef6be93cbf213
Jakub Bogacki <bogacki.jakub@gmail.com>
2014-06-05 19:06:53 +0200

    better cancellation log

--- base/src/com/github/kubatatami/judonetworking/Request.java
+++ base/src/com/github/kubatatami/judonetworking/Request.java
@@ -355,6 +355,9 @@ class Request implements Runnable, Comparable<Request>, ProgressObserver, Reques
 
     @Override
     public void cancel() {
+        if(done){
+            return;
+        }
         this.cancelled = true;
         if ((rpc.getDebugFlags() & Endpoint.CANCEL_DEBUG) > 0) {
             LoggerImpl.log("Request " + name + " cancelled.");
