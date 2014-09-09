bfe9e2ade806a7b61a56d1eedaae80bd7434ceec
Jakub Bogacki <bogacki.jakub@gmail.com>
2014-09-09 20:29:39 +0200

    okhttp creator

---
+++ base/src/com/github/kubatatami/judonetworking/transports/OkHttpCreator.java
@@ -0,0 +1,26 @@
+package com.github.kubatatami.judonetworking.transports;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Kuba on 09/09/14.
+ */
+public class OkHttpCreator implements HttpTransportLayer.HttpURLCreator {
+
+    protected OkHttpClient client = new OkHttpClient();
+    protected OkUrlFactory factory = new OkUrlFactory(client);
+
+    @Override
+    public HttpURLConnection create(String url) throws IOException {
+        return factory.open(new URL(url));
+    }
+
+    public OkHttpClient getClient() {
+        return client;
+    }
+}
