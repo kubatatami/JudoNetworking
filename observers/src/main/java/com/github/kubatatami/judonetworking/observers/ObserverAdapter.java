22a943105bfaf8f9aa8bcb1dc42322ec6bf2e36d
Jakub Bogacki <bogacki.jakub@gmail.com>
2015-06-30 14:15:11 +0200

    public getItems in ObserverAdapter

--- observers/src/main/java/com/github/kubatatami/judonetworking/observers/ObserverAdapter.java
+++ observers/src/main/java/com/github/kubatatami/judonetworking/observers/ObserverAdapter.java
@@ -204,4 +204,13 @@ public class ObserverAdapter<T> extends ArrayAdapter<T> implements Iterable<T> {
         }
     }
 
+    public List<T> getItems(){
+        try {
+            return (List<T>) mObjectsField.get(this);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
