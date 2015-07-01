933ff6ca5f284ec958589250383451b22e458630
Jakub Bogacki <bogacki.jakub@gmail.com>
2015-07-01 17:39:51 +0200

    StatefulCallback

--- base/src/main/java/com/github/kubatatami/judonetworking/callbacks/SupportFragmentCallback.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/callbacks/SupportFragmentCallback.java
@@ -1,9 +1,11 @@
 package com.github.kubatatami.judonetworking.callbacks;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
 import com.github.kubatatami.judonetworking.AsyncResult;
 import com.github.kubatatami.judonetworking.CacheInfo;
 import com.github.kubatatami.judonetworking.exceptions.JudoException;
-import com.github.kubatatami.judonetworking.fragments.JudoSupportFragment;
 
 import java.lang.ref.WeakReference;
 
@@ -13,49 +15,103 @@ import java.lang.ref.WeakReference;
  * Date: 23.04.2013
  * Time: 11:40
  */
-public final class SupportFragmentCallback<T> extends DecoratorCallback<T> {
+public class SupportFragmentCallback<T> extends DefaultCallback<T> implements FragmentManager.OnBackStackChangedListener {
 
+    private final WeakReference<Fragment> fragment;
+    private final WeakReference<FragmentManager> manager;
     private AsyncResult asyncResult;
-    private final int id;
-    private final String who;
-    private int progress;
 
-    public SupportFragmentCallback(JudoSupportFragment fragment, int id, Callback<T> callback) {
-        super(callback);
-        this.id=id;
-        this.who=fragment.getWho();
-        JudoSupportFragment.addCallback(who, id, this);
+    public SupportFragmentCallback(Fragment fragment) {
+        this.fragment = new WeakReference<>(fragment);
+        this.manager = new WeakReference<>(fragment.getFragmentManager());
+        if (manager.get() != null) {
+            manager.get().addOnBackStackChangedListener(this);
+        }
     }
 
     @Override
     public final void onStart(CacheInfo cacheInfo, AsyncResult asyncResult) {
         this.asyncResult = asyncResult;
-        super.onStart(cacheInfo, asyncResult);
+        if (fragment.get() != null && fragment.get().getActivity() != null) {
+            onSafeStart(cacheInfo, asyncResult);
+        } else {
+            tryCancel();
+        }
     }
 
     @Override
-    public void onFinish() {
-        super.onFinish();
-        JudoSupportFragment.removeCallback(who, id);
+    public final void onSuccess(T result) {
+        if (fragment.get() != null && fragment.get().getActivity() != null) {
+            onSafeSuccess(result);
+        } else {
+            tryCancel();
+        }
     }
 
     @Override
-    public void onProgress(int progress) {
-        super.onProgress(progress);
-        this.progress=progress;
+    public final void onError(JudoException e) {
+        if (fragment.get() != null && fragment.get().getActivity() != null) {
+            onSafeError(e);
+        } else {
+            tryCancel();
+        }
     }
 
-    public void tryCancel() {
+    @Override
+    public final void onProgress(int progress) {
+        if (fragment.get() != null && fragment.get().getActivity() != null) {
+            onSafeProgress(progress);
+        } else {
+            tryCancel();
+        }
+    }
+
+    @Override
+    public final void onFinish() {
+        if (fragment.get() != null && fragment.get().getActivity() != null) {
+            onSafeFinish();
+        } else {
+            tryCancel();
+        }
+        if (manager.get() != null) {
+            manager.get().removeOnBackStackChangedListener(this);
+        }
+    }
+
+    protected void tryCancel() {
         if (asyncResult != null) {
             asyncResult.cancel();
+            if (manager.get() != null) {
+                manager.get().removeOnBackStackChangedListener(this);
+            }
         }
     }
 
-    public void setCallback(Callback<?> callback){
-        this.callback=new WeakReference<>((Callback<T>) callback);
-        if(progress>0){
-            callback.onProgress(progress);
-        }
+    public void onSafeStart(CacheInfo cacheInfo, AsyncResult asyncResult) {
+
     }
 
+    public void onSafeProgress(int progress) {
+
+    }
+
+    public void onSafeSuccess(T result) {
+
+    }
+
+    public void onSafeFinish() {
+
+    }
+
+    public void onSafeError(JudoException e) {
+
+    }
+
+
+    @Override
+    public void onBackStackChanged() {
+        if (fragment.get() != null && fragment.get().getActivity() == null) {
+            tryCancel();
+        }
+    }
 }
--- base/src/main/java/com/github/kubatatami/judonetworking/fragments/JudoSupportFragment.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/fragments/JudoSupportFragment.java
@@ -2,9 +2,12 @@ package com.github.kubatatami.judonetworking.fragments;
 
 import android.support.v4.app.Fragment;
 
+import com.github.kubatatami.judonetworking.AsyncResult;
+import com.github.kubatatami.judonetworking.CacheInfo;
 import com.github.kubatatami.judonetworking.callbacks.Callback;
-import com.github.kubatatami.judonetworking.callbacks.SupportFragmentCallback;
+import com.github.kubatatami.judonetworking.callbacks.DecoratorCallback;
 
+import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +17,7 @@ import java.util.Map;
  */
 public class JudoSupportFragment extends Fragment {
 
-    private static final Map<String, Map<Integer, SupportFragmentCallback<?>>> callbacksMap = new HashMap<>();
+    private static final Map<String, Map<Integer, StatefulCallback<?>>> callbacksMap = new HashMap<>();
 
     private String mWho;
 
@@ -33,7 +36,7 @@ public class JudoSupportFragment extends Fragment {
 
     protected boolean connectCallback(int id, Callback<?> callback) {
         if (callbacksMap.containsKey(getWho())) {
-            Map<Integer, SupportFragmentCallback<?>> fragmentCallbackMap = callbacksMap.get(getWho());
+            Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(getWho());
             if (fragmentCallbackMap.containsKey(id)) {
                 fragmentCallbackMap.get(id).setCallback(callback);
                 return true;
@@ -42,20 +45,20 @@ public class JudoSupportFragment extends Fragment {
         return false;
     }
 
-    public static void addCallback(String who, int id, SupportFragmentCallback<?> supportFragmentCallback) {
+    static void addCallback(String who, int id, StatefulCallback<?> statefulCallback) {
         if (!callbacksMap.containsKey(who)) {
-            callbacksMap.put(who, new HashMap<Integer, SupportFragmentCallback<?>>());
+            callbacksMap.put(who, new HashMap<Integer, StatefulCallback<?>>());
         }
-        Map<Integer, SupportFragmentCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
+        Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
         if (fragmentCallbackMap.containsKey(id)) {
             fragmentCallbackMap.get(id).tryCancel();
         }
-        fragmentCallbackMap.put(id, supportFragmentCallback);
+        fragmentCallbackMap.put(id, statefulCallback);
     }
 
-    public static void removeCallback(String who, int id) {
+    static void removeCallback(String who, int id) {
         if (callbacksMap.containsKey(who)) {
-            Map<Integer, SupportFragmentCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
+            Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
             if (fragmentCallbackMap.containsKey(id)) {
                 fragmentCallbackMap.remove(id);
             }
@@ -63,4 +66,56 @@ public class JudoSupportFragment extends Fragment {
     }
 
 
+    /**
+     * Created with IntelliJ IDEA.
+     * User: jbogacki
+     * Date: 23.04.2013
+     * Time: 11:40
+     */
+    public static final class StatefulCallback<T> extends DecoratorCallback<T> {
+
+        private AsyncResult asyncResult;
+        private final int id;
+        private final String who;
+        private int progress;
+
+        public StatefulCallback(JudoSupportFragment fragment, int id, Callback<T> callback) {
+            super(callback);
+            this.id=id;
+            this.who=fragment.getWho();
+            addCallback(who, id, this);
+        }
+
+        @Override
+        public final void onStart(CacheInfo cacheInfo, AsyncResult asyncResult) {
+            this.asyncResult = asyncResult;
+            super.onStart(cacheInfo, asyncResult);
+        }
+
+        @Override
+        public void onFinish() {
+            super.onFinish();
+            removeCallback(who, id);
+        }
+
+        @Override
+        public void onProgress(int progress) {
+            super.onProgress(progress);
+            this.progress=progress;
+        }
+
+        public void tryCancel() {
+            if (asyncResult != null) {
+                asyncResult.cancel();
+            }
+        }
+
+        public void setCallback(Callback<?> callback){
+            this.callback=new WeakReference<>((Callback<T>) callback);
+            if(progress>0){
+                callback.onProgress(progress);
+            }
+        }
+
+    }
 }
