cfc4cf95c0e35b5202c7c8a4be24608edd883211
Jakub Bogacki <bogacki.jakub@gmail.com>
2015-07-02 10:48:01 +0200

    StatefulBatch

--- base/src/main/java/com/github/kubatatami/judonetworking/fragments/JudoSupportFragment.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/fragments/JudoSupportFragment.java
@@ -4,8 +4,11 @@ import android.support.v4.app.Fragment;
 
 import com.github.kubatatami.judonetworking.AsyncResult;
 import com.github.kubatatami.judonetworking.CacheInfo;
+import com.github.kubatatami.judonetworking.batches.Batch;
+import com.github.kubatatami.judonetworking.batches.DecoratorBatch;
 import com.github.kubatatami.judonetworking.callbacks.Callback;
 import com.github.kubatatami.judonetworking.callbacks.DecoratorCallback;
+import com.github.kubatatami.judonetworking.exceptions.JudoException;
 
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
@@ -17,11 +20,11 @@ import java.util.Map;
  */
 public class JudoSupportFragment extends Fragment {
 
-    private static final Map<String, Map<Integer, StatefulCallback<?>>> callbacksMap = new HashMap<>();
+    private static final Map<String, Map<Integer, Stateful>> callbacksMap = new HashMap<>();
 
     private String mWho;
 
-    public String getWho() {
+    private String getWho() {
         if (mWho == null) {
             try {
                 Field whoFiled = Fragment.class.getDeclaredField("mWho");
@@ -34,13 +37,28 @@ public class JudoSupportFragment extends Fragment {
         return mWho;
     }
 
+    private void removeCallbacks(String who) {
+        if (callbacksMap.containsKey(who)) {
+            Map<Integer, Stateful> fragmentCallbackMap = callbacksMap.get(who);
+            for(Map.Entry<Integer, Stateful> entry : fragmentCallbackMap.entrySet()){
+                entry.getValue().setCallback(null);
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        removeCallbacks(getWho());
+    }
+
     protected boolean connectCallback(Callback<?> callback) {
         return connectCallback(callback.getClass().hashCode(),callback);
     }
 
     protected boolean connectCallback(int id, Callback<?> callback) {
         if (callbacksMap.containsKey(getWho())) {
-            Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(getWho());
+            Map<Integer, Stateful> fragmentCallbackMap = callbacksMap.get(getWho());
             if (fragmentCallbackMap.containsKey(id)) {
                 fragmentCallbackMap.get(id).setCallback(callback);
                 return true;
@@ -57,20 +75,28 @@ public class JudoSupportFragment extends Fragment {
         return new StatefulCallback<>(this,id, callback);
     }
 
-    static void addCallback(String who, int id, StatefulCallback<?> statefulCallback) {
+    protected <T> StatefulBatch<T> generateCallback(Batch<T> batch) {
+        return new StatefulBatch<>(this, batch);
+    }
+
+    protected <T> StatefulBatch<T> generateCallback(int id, Batch<T> batch) {
+        return new StatefulBatch<>(this,id, batch);
+    }
+
+    static void addStatefulCallback(String who, int id, Stateful statefulCallback) {
         if (!callbacksMap.containsKey(who)) {
-            callbacksMap.put(who, new HashMap<Integer, StatefulCallback<?>>());
+            callbacksMap.put(who, new HashMap<Integer, Stateful>());
         }
-        Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
+        Map<Integer, Stateful> fragmentCallbackMap = callbacksMap.get(who);
         if (fragmentCallbackMap.containsKey(id)) {
             fragmentCallbackMap.get(id).tryCancel();
         }
         fragmentCallbackMap.put(id, statefulCallback);
     }
 
-    static void removeCallback(String who, int id) {
+    static void removeStatefulCallback(String who, int id) {
         if (callbacksMap.containsKey(who)) {
-            Map<Integer, StatefulCallback<?>> fragmentCallbackMap = callbacksMap.get(who);
+            Map<Integer, Stateful> fragmentCallbackMap = callbacksMap.get(who);
             if (fragmentCallbackMap.containsKey(id)) {
                 fragmentCallbackMap.remove(id);
             }
@@ -78,18 +104,22 @@ public class JudoSupportFragment extends Fragment {
     }
 
 
-    /**
-     * Created with IntelliJ IDEA.
-     * User: jbogacki
-     * Date: 23.04.2013
-     * Time: 11:40
-     */
-    public static final class StatefulCallback<T> extends DecoratorCallback<T> {
+    interface Stateful<T>{
+        void setCallback(T callback);
+        void tryCancel();
+    }
+
+
+
+    public static final class StatefulCallback<T> extends DecoratorCallback<T> implements Stateful<Callback<?>> {
 
         private AsyncResult asyncResult;
         private final int id;
         private final String who;
         private int progress;
+        private boolean consume=false;
+        private T data;
+        private JudoException exception;
 
         public StatefulCallback(JudoSupportFragment fragment, Callback<T> callback) {
             this(fragment, callback.getClass().hashCode(), callback);
@@ -99,19 +129,23 @@ public class JudoSupportFragment extends Fragment {
             super(callback);
             this.id = id;
             this.who = fragment.getWho();
-            addCallback(who, id, this);
+            addStatefulCallback(who, id, this);
         }
 
         @Override
         public final void onStart(CacheInfo cacheInfo, AsyncResult asyncResult) {
             this.asyncResult = asyncResult;
+            consume=false;
+            data=null;
+            exception=null;
             super.onStart(cacheInfo, asyncResult);
         }
 
         @Override
         public void onFinish() {
             super.onFinish();
-            removeCallback(who, id);
+            removeStatefulCallback(who, id);
+            consume=true;
         }
 
         @Override
@@ -120,16 +154,94 @@ public class JudoSupportFragment extends Fragment {
             this.progress = progress;
         }
 
+        @Override
         public void tryCancel() {
             if (asyncResult != null) {
                 asyncResult.cancel();
             }
         }
 
+        @Override
         public void setCallback(Callback<?> callback) {
             this.callback = new WeakReference<>((Callback<T>) callback);
             if (progress > 0) {
                 callback.onProgress(progress);
+            }
+            if(!consume){
+                if(data!=null){
+                    this.callback.get().onSuccess(data);
+                }else if(exception!=null){
+                    this.callback.get().onError(exception);
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public static final class StatefulBatch<T> extends DecoratorBatch<T> implements Stateful<Batch<?>>{
+
+        private AsyncResult asyncResult;
+        private final int id;
+        private final String who;
+        private int progress;
+        private boolean consume=false;
+        private Object[] data;
+        private JudoException exception;
+
+        public StatefulBatch(JudoSupportFragment fragment, Batch<T> batch) {
+            this(fragment, batch.getClass().hashCode(), batch);
+        }
+
+        public StatefulBatch(JudoSupportFragment fragment, int id, Batch<T> batch) {
+            super(batch);
+            this.id = id;
+            this.who = fragment.getWho();
+            addStatefulCallback(who, id, this);
+        }
+
+        @Override
+        public final void onStart(AsyncResult asyncResult) {
+            this.asyncResult = asyncResult;
+            consume=false;
+            data=null;
+            exception=null;
+            super.onStart(asyncResult);
+        }
+
+        @Override
+        public void onFinish() {
+            super.onFinish();
+            removeStatefulCallback(who, id);
+            consume=true;
+        }
+
+        @Override
+        public void onProgress(int progress) {
+            super.onProgress(progress);
+            this.progress = progress;
+        }
+
+        @Override
+        public void tryCancel() {
+            if (asyncResult != null) {
+                asyncResult.cancel();
+            }
+        }
+
+        public void setCallback(Batch<?> batch) {
+            this.batch = new WeakReference<>((Batch<T>) batch);
+            if (progress > 0) {
+                batch.onProgress(progress);
+            }
+            if(!consume){
+                if(data!=null){
+                    this.batch.get().onSuccess(data);
+                }else if(exception!=null){
+                    this.batch.get().onError(exception);
+                }
             }
         }
 
