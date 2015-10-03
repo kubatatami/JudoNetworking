34fa59016e95608163936c1d65f210446b5188b1
Jakub Bogacki <bogacki.jakub@gmail.com>
2015-10-03 17:33:01 +0200

    improve merge callback

--- base/src/main/java/com/github/kubatatami/judonetworking/batches/DefaultBatch.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/batches/DefaultBatch.java
@@ -57,7 +57,7 @@ public abstract class DefaultBatch<T> implements Batch<T>, AsyncResultCallback {
     @Override
     public void onProgress(int progress) {
         if (mergeCallback != null) {
-            mergeCallback.addProgress(progress);
+            mergeCallback.addProgress(this, progress);
         }
     }
 
