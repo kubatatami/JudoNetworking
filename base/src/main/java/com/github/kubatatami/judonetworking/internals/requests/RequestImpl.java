1617147e50c679b7cf159d7e3f63609df860ce3c
Jakub Bogacki <bogacki.jakub@gmail.com>
2016-09-01 11:29:41 +0200

    fix request counter

--- base/src/main/java/com/github/kubatatami/judonetworking/internals/requests/RequestImpl.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/internals/requests/RequestImpl.java
@@ -393,7 +393,7 @@ public class RequestImpl implements Runnable, Comparable<RequestImpl>, ProgressO
 
     @Override
     public void cancel() {
-        if (done) {
+        if (done || cancelled) {
             return;
         }
         this.cancelled = true;
