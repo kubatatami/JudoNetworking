1695f764ed9ad98f7b0496af2d470089c3460c44
Jakub Bogacki <bogacki.jakub@gmail.com>
2016-10-01 20:19:18 +0200

    oauth fixes

--- base/src/main/java/com/github/kubatatami/judonetworking/internals/AsyncResultSender.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/internals/AsyncResultSender.java
@@ -146,9 +146,6 @@ public class AsyncResultSender implements Runnable {
             if (request.isCancelled()) {
                 return;
             }
-            if (type == Type.RESULT || type == Type.ERROR) {
-                request.done();
-            }
             switch (type) {
                 case START:
                     request.start();
@@ -179,14 +176,12 @@ public class AsyncResultSender implements Runnable {
             }
             if (type == Type.RESULT || type == Type.ERROR) {
                 callback.onFinish();
+                request.done();
             }
         } else if (requestProxy != null && requestProxy.getBatchCallback() != null) {
             Batch<?> transaction = requestProxy.getBatchCallback();
             if (requestProxy.isCancelled()) {
                 return;
-            }
-            if (type == Type.RESULT || type == Type.ERROR) {
-                requestProxy.done();
             }
             switch (type) {
                 case START:
@@ -218,8 +213,8 @@ public class AsyncResultSender implements Runnable {
             }
             if (type == Type.RESULT || type == Type.ERROR) {
                 transaction.onFinish();
+                requestProxy.done();
                 requestProxy.clearBatchCallback();
-
             }
         } else if (requests != null && type == Type.PROGRESS) {
             for (RequestImpl batchRequest : requests) {
