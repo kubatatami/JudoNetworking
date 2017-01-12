27bc30cb72fb55ab1369a500f09dd7e41cff5de1
Jakub Bogacki <bogacki.jakub@gmail.com>
2017-01-12 13:18:32 +0100

    Remove tags and @DataObserver

--- observers/src/main/java/com/github/kubatatami/judonetworking/activities/ObserverActivity.java
+++ observers/src/main/java/com/github/kubatatami/judonetworking/activities/ObserverActivity.java
@@ -1,8 +1,5 @@
 package com.github.kubatatami.judonetworking.activities;
 
-import android.os.Bundle;
-import android.view.ViewGroup;
-
 import com.github.kubatatami.judonetworking.observers.ObservableController;
 import com.github.kubatatami.judonetworking.observers.ObservableWrapper;
 import com.github.kubatatami.judonetworking.observers.ObserverHelper;
@@ -16,25 +13,7 @@ import com.github.kubatatami.judonetworking.observers.WrapperObserver;
  */
 public class ObserverActivity extends JudoActivity implements ObservableController {
 
-    private ObserverHelper observerHelper;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        observerHelper = new ObserverHelper(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        observerHelper.stop();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        observerHelper.start(this, ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0));
-    }
+    private ObserverHelper observerHelper = new ObserverHelper();
 
     @Override
     protected void onDestroy() {
