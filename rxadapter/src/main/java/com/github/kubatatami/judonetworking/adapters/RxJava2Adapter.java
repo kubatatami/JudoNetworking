2ca8e198044420e045ac3edd525960f6f21e8413
Jakub Bogacki <bogacki.jakub@gmail.com>
2019-05-09 13:46:22 +0200

    remove wear, update libs, improve RxJava2Adapter

--- rxadapter/src/main/java/com/github/kubatatami/judonetworking/adapters/RxJava2Adapter.java
+++ rxadapter/src/main/java/com/github/kubatatami/judonetworking/adapters/RxJava2Adapter.java
@@ -10,6 +10,8 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.functions.Action;
 import io.reactivex.subjects.PublishSubject;
 
 @SuppressWarnings("unchecked")
@@ -17,24 +19,45 @@ public class RxJava2Adapter implements JudoAdapter {
 
     @Override
     public boolean canHandle(Type type) {
-        return type instanceof ParameterizedType && ((ParameterizedType) type).getRawType().equals(Observable.class);
+        return type instanceof ParameterizedType && isSupportedType(((ParameterizedType) type).getRawType());
+    }
+
+    private boolean isSupportedType(Type type) {
+        return type.equals(Observable.class) || type.equals(Single.class);
     }
 
     @Override
     public MethodInfo getMethodInfo(Type returnType, Object[] args, Type[] types) {
-        final PublishSubject<RxRequestStatus> subject = PublishSubject.create();
         ParameterizedType observableType = (ParameterizedType) ((ParameterizedType)returnType).getActualTypeArguments()[0];
-        Type resultType = observableType.getActualTypeArguments()[0];
-        return new MethodInfo(new SubjectCallback(subject), resultType, args, subject);
+        if (observableType.getRawType().equals(RxRequestStatus.class)) {
+            Type resultType = observableType.getActualTypeArguments()[0];
+            RxRequestStatusSubjectCallback callback = new RxRequestStatusSubjectCallback();
+            return new MethodInfo(callback, resultType, args, prepareReturnObject(callback.observable, returnType));
+        } else {
+            Type resultType = observableType.getRawType();
+            SimpleSubjectCallback callback = new SimpleSubjectCallback();
+            return new MethodInfo(callback, resultType, args, prepareReturnObject(callback.observable, returnType));
+        }
+
     }
 
-    private static class SubjectCallback extends DefaultCallback {
-
-        private final PublishSubject<RxRequestStatus> subject;
-
-        private SubjectCallback(PublishSubject<RxRequestStatus> subject) {
-            this.subject = subject;
+    private Object prepareReturnObject(Observable subject, Type observableType) {
+        if (observableType.equals(Observable.class)) {
+            return subject;
+        } else {
+            return subject.firstOrError();
         }
+    }
+
+    private static class RxRequestStatusSubjectCallback extends DefaultCallback {
+
+        private final PublishSubject<RxRequestStatus> subject = PublishSubject.create();
+        final Observable<RxRequestStatus> observable = subject.doOnDispose(new Action() {
+            @Override
+            public void run() {
+                if (getAsyncResult() != null) getAsyncResult().cancel();
+            }
+        });
 
         @Override
         public void onStart(CacheInfo cacheInfo, AsyncResult asyncResult) {
@@ -60,6 +83,32 @@ public class RxJava2Adapter implements JudoAdapter {
         @Override
         public void onProgress(int progress) {
             subject.onNext(new RxRequestStatus(getAsyncResult(), progress));
+        }
+    }
+
+    private static class SimpleSubjectCallback extends DefaultCallback {
+
+        private final PublishSubject<Object> subject = PublishSubject.create();
+        final Observable<Object> observable = subject.doOnDispose(new Action() {
+            @Override
+            public void run() {
+                if (getAsyncResult() != null) getAsyncResult().cancel();
+            }
+        });
+
+        @Override
+        public void onSuccess(Object result) {
+            subject.onNext(result);
+        }
+
+        @Override
+        public void onError(JudoException e) {
+            subject.onError(e);
+        }
+
+        @Override
+        public void onFinish() {
+            subject.onComplete();
         }
     }
 }
