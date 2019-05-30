2ddb256046f2a62f3eda029f996ee1fae686cb27
Jakub Bogacki <bogacki.jakub@gmail.com>
2019-05-30 15:24:44 +0200

    wip

--- base/src/main/java/com/github/kubatatami/judonetworking/internals/RequestProxy.java
+++ base/src/main/java/com/github/kubatatami/judonetworking/internals/RequestProxy.java
@@ -645,9 +645,6 @@ public class RequestProxy implements InvocationHandler, AsyncResult {
 
     public void clearBatchCallback() {
         batchCallback = null;
-        for (Request request : batchRequests) {
-            rpc.stopRequest(request);
-        }
     }
 
     public void calcTime() {
